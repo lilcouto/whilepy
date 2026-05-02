e808b4ee2e6cbd7c56c4e06f2ff646f46f35e83b
luiz henrique oliveira coutinho <luizhenrique0647@gmail.com>
2026-05-02 19:38:55 -0300

    correção atividade 4

--- 4.py
+++ 4.py
@@ -1,10 +1,13 @@
 '''Peça números ao usuário e some-os. O programa deve parar quando o usuário digitar um número
 negativo. Ao final, mostre a soma total.'''
+resultado = 0
+
 while True:
-    n1 = int(input("Digite um número positivo: "))
-    n2 = int(input("Digite um número positivo: "))
-    if n1 > 0 and n2 > 0:
-        soma = n1 + n2
-    else: 
-        print(soma)
+    n = int(input("Digite um número: "))
+    
+    if n < 0:
         break
+    
+    resultado = resultado + n
+
+print(resultado)
